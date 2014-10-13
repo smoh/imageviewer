3754e8570df1351b074019858d0a0e2af55eb50c
Semyeong Oh <semyeong.oh@gmail.com>
2014-10-12 22:48:30 -0400

    add some features

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from astropy.table import Table
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-IMGDIR="http://www.astro.princeton.edu/~semyeong/images/"
+IMGDIR="http://www.astro.princeton.edu/~semyeong/projects/ETGOutskirts/test2/fig/image/"
 table = Table.read('s.fits')
 
 @app.route('/')
@@ -15,7 +15,12 @@ def hello_world():
 def image_view(ind=None):
     return render_template(
             'imageview.html', ind=ind, IMGDIR=IMGDIR, iauname=table['IAUNAME'][ind])
-    
+
+@app.route('/goto', methods=['POST', 'GET'])    
+def goto():
+    # error = None
+    # if request.method == 'POST':
+    return redirect('/' + request.form['index'])
 
 if __name__ == '__main__':
     app.run(debug=True)
