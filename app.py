a035bd354d2ffac1cca4971005b068d1ce2fec03
Semyeong Oh <semyeong.oh@gmail.com>
2014-10-13 01:07:23 -0400

    udpate

--- app.py
+++ app.py
@@ -4,10 +4,29 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-IMGDIRS = [
-    "http://www.astro.princeton.edu/~semyeong/projects/ETGOutskirts/test2/fig/image/",
-    "http://www.astro.princeton.edu/~semyeong/projects/ETGOutskirts/test2/fig/profile/"]
+imgdir = "http://www.astro.princeton.edu/~semyeong/projects/ETGOutskirts/test2/fig/image/"
+profdir = "http://www.astro.princeton.edu/~semyeong/projects/ETGOutskirts/test2/fig/profile/"
 table = Table.read('s.fits')
+
+class Pager(object):
+    def __init__(self, count):
+        self.count = count
+        self.current = 0
+
+    @property
+    def next(self):
+        n = self.current + 1
+        if n > self.count-1:
+            n -= self.count
+        return n
+
+    @property
+    def prev(self):
+        n = self.current - 1
+        if n < 0 :
+            n += self.count
+        return n
+pager = Pager(len(table))
 
 @app.route('/')
 def hello_world():
@@ -15,8 +34,18 @@ def hello_world():
 
 @app.route('/<int:ind>/')
 def image_view(ind=None):
-    return render_template(
-            'imageview.html', ind=ind, IMGDIRS=IMGDIRS, iauname=table['IAUNAME'][ind])
+    if ind >= len(table):
+        return "invalid index"
+    else:
+        pager.current = ind
+        return render_template(
+                'imageview.html',
+                ind=ind,
+                imgdir=imgdir,
+                profdir=profdir,
+                iauname=table['IAUNAME'][ind],
+                subdir=table['SUBDIR'][ind],
+                pager=pager)
 
 @app.route('/goto', methods=['POST', 'GET'])    
 def goto():
